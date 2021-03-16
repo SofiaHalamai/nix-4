089c01596a3164b334d20a75ec3b50b093d87b45
SofiaHalamai <xa12850327@student.karazin.ua>
2021-03-16 17:18:56 +0200

    task 3

---
+++ unit_3_oop/run.bat
@@ -0,0 +1,13 @@
+@echo off
+@echo .
+@echo ---------MVN project (my version java - 11)---------
+@echo .
+@echo ---------Delete build and create jar file---------
+@echo .
+cd .\app_calc
+call mvn clean install
+@echo .
+@echo ---------Run program---------
+@echo .
+java -jar target/app_calc-1.0-SNAPSHOT.jar
+pause
